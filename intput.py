50a6285b6af51850c60b3991884b3ec58c4ea83a
AhmedZia-crypto <zia14fox@gmail.com>
2025-08-19 13:38:25 +0300

    Add files via upload

---
+++ intput.py
@@ -0,0 +1,11 @@
+# Taking input fron user
+
+x = int(input(" enter the first number: "))
+
+y = int(input(" enter the second number: "))
+
+if x%y == 0:
+    print("it is divisible")
+
+else:
+    print("it is divisible")
